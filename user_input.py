1a6e8a95a048742621c666052ac3d94f2d5def0e
Collins <ochiengcollinsoduor2@gmail.com>
2024-02-26 01:11:00 +0300

    python assignment

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# Get user name, age  & location using input():
+name = input("What's your name? ")
+age = input("Enter your age: ")
+location = input("Where are you located(Location)? ")
+
+print(f"\nHello {name}, you are {age} years old and lives in {location}.")
